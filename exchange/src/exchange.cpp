9ec344f1e864cc4241ded4d7c0e7f43c1b1451eb
George-LastName <gfbowron@gmail.com>
2026-05-24 22:25:47 +0100

    Moved Local testing behind a compile flag.

--- exchange/src/exchange.cpp
+++ exchange/src/exchange.cpp
@@ -31,12 +31,21 @@ ItchServer::ItchServer(/*const ItchConfig& config*/){
 
     // Set when testing trader and exchange on one machine.
 #ifdef LOCAL_TESTING
+    int reuse = 1;
+    if (setsockopt(socket_, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
+        throw std::runtime_error("Failed to set SO_REUSEADDR on Itch Server.\n");
+    }
+
     struct in_addr local_interface{};
     local_interface.s_addr = inet_addr("127.0.0.1");
     if(setsockopt(socket_, IPPROTO_IP, IP_MULTICAST_IF, &local_interface, sizeof(local_interface)) < 0){
         throw std::runtime_error("Failed to set IP_MULTICAST_IF\n");
     }
 #endif //LOCAL_TESTING
+
+    if (bind(socket_, (struct sockaddr*)&destination_, sizeof(destination_)) < 0) {
+        throw std::runtime_error(std::format("Failed to Bind Itch Server.\nErrno={}\n{}", errno, strerror(errno)));
+    }
     int outcome = inet_pton(address_family, multicast_group, &destination_.sin_addr);
     if(outcome < 1){
         throw std::runtime_error(std::format("Failed to convert network address -> {}\n0: invalid network address provided.\n-1:No valid address family\n", outcome));
